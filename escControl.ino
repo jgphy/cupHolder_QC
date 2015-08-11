c40ffbe3a1c1c20836ec58837f438ea46737da8d
juansguerra <Juan@Juan-Guerras-MacBook-Pro-2.local>
2015-08-11 19:53:09 -0400

    added all the arduino files that were on google drive

---
+++ escControl.ino
@@ -0,0 +1,26 @@
+
+#include <Servo.h>
+
+
+int value = 40; // set values you need to zero
+
+Servo m1; //Create as much as Servoobject you want. You can controll 2 or more Servos at the same time
+
+void setup() {
+
+  m1.attach(9);    // attached to pin 9 I just do this with 1 Servo
+  Serial.begin(9600);    // start serial at 9600 baud
+  m1.write(value);
+}
+
+void loop() {
+
+//First connect your ESC WITHOUT Arming. Then Open Serial and follo Instructions
+ 
+  m1.write(value);
+ 
+  if(Serial.available()) 
+    value = Serial.parseInt();    // Parse an Integer from Serial
+
+}
+
---
+++ gyroTest/gyroTest.ino
@@ -0,0 +1,26 @@
+#include <Adafruit_L3GD20.h>
+
+
+// No need to specify pins for I2C
+Adafruit_L3GD20 gyro;
+
+void setup() 
+{
+  Serial.begin(9600);
+  
+  // Try to initialise and warn if we couldn't detect the chip
+  if (!gyro.begin(gyro.L3DS20_RANGE_250DPS))
+  {
+    Serial.println("Oops ... unable to initialize the L3GD20. Check your wiring!");
+    while (1);
+  }
+}
+
+void loop() 
+{
+  gyro.read();
+  Serial.print("X: "); Serial.print((int)gyro.data.x);   Serial.print(" ");
+  Serial.print("Y: "); Serial.print((int)gyro.data.y);   Serial.print(" ");
+  Serial.print("Z: "); Serial.println((int)gyro.data.z); Serial.print(" ");
+  delay(100);
+}
