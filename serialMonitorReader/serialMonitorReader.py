fad57cfe10183c490c01ffb42193f5361766104d
jgphy <juan221guerra@gmail.com>
2016-01-15 16:27:19 -0500

    fixed serial monitor reader so that their is no delay. Still needs to be modified so that it can accept multiple sets of data at a time.

--- serialMonitorReader/serialMonitorReader.py
+++ serialMonitorReader/serialMonitorReader.py
@@ -1,45 +1,36 @@
 import serial
 import matplotlib.pyplot as plt
 import numpy as np
+from drawnow import *
 
 
+def figure(y):
+    plt.plot(y,'ro-'
 
 def main():
 
-    ser = serial.Serial('/dev/cu.usbmodem1411', 9600)
+    ser = serial.Serial('/dev/cu.usbmodem1411',115200)
     x=list()
     y=list()
     plt.ion() ## Note this correction
-    fig=plt.figure()        
-    plt.axis([0,1000,-2,2])    
+    # fig=plt.figure()        
+    # plt.axis([0,1000,-2,2])    
     i=0
-
-    q = ""
-    r = []
-    s = []
-
-    color_list = plt.cm.Set3(np.linspace(0, 1, 12))
-
-
-    for butts in range(20):
-
-        ser.readline()
-
     while(1):
+        while ser.inWaiting==0:
+            pass
         q = ser.readline()
-        r = q.split(';')
-        #print(r)
-        for j in range(len(r)):
-            try:
-                temp_y = r[j]
-                x.append(i+5)
-                y.append(temp_y)
-                plt.scatter(i,temp_y, color = color_list[j])
-                plt.show()
-                plt.pause(0.0001) #Note this correction
-                i+=1
-            except ValueError:
-                print "Oops!  That was no valid number.  Try again..."
+
+        temp_y = r[j]
+        # x.append(i+5)
+        y.append(temp_y)
+        drawnow(figure)
+        # plt.show()
+        plt.pause(0.000001)
+        i+=1
+        if i>50:
+            # x.pop(0)
+            y.pop(0)
 
 
 main()
